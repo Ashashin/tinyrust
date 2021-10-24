7b15c00d2a07e4622bb7ec1e1d600b41284d80a5
AntoineHus <AntoineHus@users.noreply.github.com>
2021-10-24 14:31:08 +0200

    Update collatz.asm
    Translate comment in English

--- assets/collatz.asm
+++ assets/collatz.asm
@@ -1,30 +1,30 @@
 ; TinyRAM V=1.000 W=63 K=8
 ;;; Collatz
 _init:
-read r0, 0                  ; Lis la valeur d'entrée
-mov r4, 0                   ; chargement du compteur d'itérations à zéro
+read r0, 0                  ; Read an entry value from a tape file
+mov r4, 0                   ; init the iteration counter to 0 
 
 _loop:                      
-    cmpe r0, 1              ; calcul du critère d'arrêt de la boucle (on arrive sur la suite 4,2,1 )
+    cmpe r0, 1              ; Loop out criteria
     cjmp _end               ; 
-    mov  r1, r0              ;
-    umod r2, r1, 2          ; on checke si r0 est pair
-    cmpe r2, 0
+    mov  r1, r0             ;
+    umod r2, r1, 2          ; check if r1 is even
+    cmpe r2, 0              ;
     cjmp _even              ;
     jmp  _odd               ;
 
 _even:                      
-    shr r0, r1, 1           ; on divise r0 par 2
+    shr r0, r1, 1           ;  r0 divise by 2
     add r4, r4, 1           ;
     jmp _loop               ;
 
 _odd:                                     
     mull r3, r1, 3          ; r3 = r0 * 3
-    add  r3, r3, 1           ; r3++ 
-    mov  r0, r3              ;
-    add  r4, r4, 1           ;
-    jmp  _loop               ;
+    add  r3, r3, 1          ; r3++ 
+    mov  r0, r3             ;
+    add  r4, r4, 1          ;
+    jmp  _loop              ;
 
 _end:  
-    store  r0, r4 
+    store  r0, r4           ;
     answer 0             
