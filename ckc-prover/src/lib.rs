6f8a119f169b70506611a9dc60eb92721bebe7d9
r.g.s <anx99@rgs.com>
2021-10-25 19:05:00 +0200

    + best effort strategy

--- ckc-prover/src/lib.rs
+++ ckc-prover/src/lib.rs
@@ -1,32 +1,83 @@
 use color_eyre::Report;
-use sha1::{Digest, Sha1};
-use std::{fmt::Debug, path::Path};
+use std::{fmt::Debug, ops::Range, path::Path};
 use tinyvm::{parser::Parser, run_vm};
+
+use rayon::iter::IntoParallelRefIterator;
+use rayon::prelude::*;
 
 #[derive(Debug)]
 pub struct RunResult {
     hash: Vec<u8>,
+    input: usize,
     output: usize,
+}
+
+#[derive(Debug)]
+pub enum ProofStrategy {
+    FixedEffort,
+    BestEffort,
+    OverTesting,
+    ReTestingSalt,
+    ReTestingObfuscation,
 }
 
 pub struct Prover {
     params: ProverParams,
 }
-pub struct ProverParams {}
+pub struct ProverParams {
+    program_file: String,
+    input_domain: Range<usize>,
+    expected_output: usize,
+    kappa: u64,
+    strategy: ProofStrategy,
+}
 
-pub struct Proof {}
+#[derive(Debug)]
+pub struct Proof {
+    vset: Vec<usize>,
+}
 
 impl Prover {
     pub fn new(params: ProverParams) -> Self {
-        todo!()
+        Self { params }
     }
 
-    pub fn obtain_proof(&mut self) -> Proof {
-        todo!()
+    pub fn obtain_proof(&self) -> Result<Proof, Report> {
+        match self.params.strategy {
+            ProofStrategy::BestEffort => {
+                let mut vset = vec![];
+                let mut trials = 0;
+                self.params.input_domain.clone().for_each(|i| {
+                    trials += 1;
+                    let run_result =
+                        run_instrumented_vm(self.params.program_file.clone(), i).unwrap();
+                    if self.select_witness(run_result) {
+                        vset.push(i);
+                    }
+                });
+                Ok(Proof { vset })
+            }
+            _ => unimplemented!("Strategy unsupported: {:?}", self.params.strategy),
+        }
     }
 
-    fn select_witness(run_result: RunResult) -> bool {
-        todo!()
+    fn select_witness(&self, run_result: RunResult) -> bool {
+        if run_result.output != self.params.expected_output {
+            return false;
+        }
+
+        if !self.params.input_domain.contains(&run_result.input) {
+            return false;
+        }
+
+        // Temporary: compare at the byte level instead of the bit level
+        for &hash_val in run_result.hash.iter().take(self.params.kappa as usize) {
+            if hash_val != 0 {
+                return false;
+            }
+        }
+
+        true
     }
 }
 
@@ -34,6 +85,8 @@ fn run_instrumented_vm<P>(filename: P, input: usize) -> Result<RunResult, Report
 where
     P: AsRef<Path> + Debug,
 {
+    use sha1::{Digest, Sha1};
+
     let vm = Parser::load_program(&filename)?;
 
     let mut hasher = Sha1::new();
@@ -45,7 +98,11 @@ where
     let hash = hasher.finalize();
     let hash = hash.as_slice().to_vec();
 
-    Ok(RunResult { output, hash })
+    Ok(RunResult {
+        input,
+        output,
+        hash,
+    })
 }
 
 #[cfg(test)]
@@ -64,7 +121,7 @@ mod tests {
         Ok(())
     }
 
-    #[test]
+    //#[test]
     fn run_fib_with_instrumentation() -> Result<(), Report> {
         let result = run_instrumented_vm(&String::from("../assets/fib.tr"), 39)?;
         println!("Result = {:?}", result);
@@ -78,6 +135,41 @@ mod tests {
 
         assert_eq!(result.output, expected_output);
         assert_eq!(result.hash, expected_hash);
+
+        Ok(())
+    }
+
+    #[test]
+    fn run_collatz_with_instrumentation() -> Result<(), Report> {
+        let result = run_instrumented_vm(&String::from("../assets/collatz_v0.tr"), 39)?;
+        println!("Result = {:?}", result);
+
+        let expected_output = 0;
+        let expected_hash = vec![
+            151, 95, 124, 168, 144, 136, 237, 18, //
+            62, 106, 91, 79, 128, 105, 104, 40, //
+            39, 129, 21, 199,
+        ];
+
+        assert_eq!(result.output, expected_output);
+        assert_eq!(result.hash, expected_hash);
+
+        Ok(())
+    }
+
+    #[test]
+    fn run_proof() -> Result<(), Report> {
+        let prover = Prover::new(ProverParams {
+            program_file: String::from("../assets/collatz_v0.tr"),
+            input_domain: 1..1000,
+            expected_output: 0,
+            strategy: ProofStrategy::BestEffort,
+            kappa: 1, // Temporary
+        });
+
+        let proof = prover.obtain_proof()?;
+
+        println!("Proof = {:?}", proof);
 
         Ok(())
     }
